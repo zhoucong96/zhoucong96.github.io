8d579702b884980ea453afd3b971d003b01701aa
zhoucong96 <zhoucong961017@gmail.com>
2016-03-05 00:08:45 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,15 @@
-console.log('This would be the main JS file.');
+$(document).ready(function(){
+    $('header').mouseenter(function(){
+        $(this).animate({
+           height: '+=10px'
+       });
+    });
+    $('header').mouseleave(function(){
+        $(this).animate({
+           height: '-=10px'
+       });
+    })
+    $('div').click(function() {
+       $(this).hide();
+   }); 
+});
